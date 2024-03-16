ac31da2842c72cf286f958039096b1516b6e84bc
Urilk <lucaslamboley@icloud.com>
2024-03-16 16:08:36 +0100

    Remove s
    Signed-off-by: Urilk <lucaslamboley@icloud.com>

---
+++ Config/Settings.lua
@@ -0,0 +1,35 @@
+local T, C, L = unpack((select(2, ...)))
+
+C['Profile'] = {
+    ['Maldeth'] = {
+        [265] = 'pvp', -- Affliction
+        [266] = 'pvp', -- Demonology
+        [267] = 'pvp', -- Destruction
+    },
+    ['Tyniaril'] = {
+        [62] = 'pvp', -- Arcane
+        [63] = 'pvp', -- Fire
+        [64] = 'pvp', -- Frost
+    },
+    ['Bungorm'] = {
+        [256] = 'pve', -- Discipline
+        [257] = 'pve', -- Holy
+        [258] = 'pvp', -- Shadow
+    },
+    ['Paetun'] = {
+        [262] = 'pvp', -- Elemental
+        [263] = 'pvp', -- Enhancement
+        [264] = 'pve', -- Restoration
+    },
+    ['Himitosh'] = {
+        [268] = 'pve', -- Brewmaster
+        [270] = 'pve', -- Mistweaver
+        [269] = 'pvp', -- Windwalker
+    },
+    ['Mirkot'] = {
+        [102] = 'pve', -- Balance
+        [103] = 'pve', -- Feral
+        [104] = 'pve', -- Guardian
+        [105] = 'pve', -- Restoration
+    },
+}
--- Core/Loading.lua
+++ Core/Loading.lua
@@ -5,7 +5,7 @@ local Loading = CreateFrame('Frame')
 function Loading:OnEvent(event)
 	if (event == 'PLAYER_LOGIN') then
 		T['Miscellaneous']:Enable()
-		T['Profiles']:Enable()
+		T['Profile']:Enable()
 	end
 end
 
