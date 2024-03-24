4fb86b5496c504563060c79bec990315077b6f1e
llamboley <lucaslamboley@icloud.com>
2024-03-24 02:03:41 +0100

    Use dps instead of pve
    Signed-off-by: llamboley <lucaslamboley@icloud.com>

--- Config/Settings.lua
+++ Config/Settings.lua
@@ -2,41 +2,41 @@ local T, C, L = unpack((select(2, ...)))
 
 C['Profile'] = {
     ['Maldeth'] = {
-        [265] = 'pvp', -- Affliction
-        [266] = 'pvp', -- Demonology
-        [267] = 'pvp', -- Destruction
+        [265] = 'dps', -- Affliction
+        [266] = 'dps', -- Demonology
+        [267] = 'dps', -- Destruction
     },
     ['Tyniaril'] = {
-        [62] = 'pvp', -- Arcane
-        [63] = 'pvp', -- Fire
-        [64] = 'pvp', -- Frost
+        [62] = 'dps', -- Arcane
+        [63] = 'dps', -- Fire
+        [64] = 'dps', -- Frost
     },
     ['Bungorm'] = {
-        [256] = 'pve', -- Discipline
-        [257] = 'pve', -- Holy
-        [258] = 'pvp', -- Shadow
+        [256] = 'heal', -- Discipline
+        [257] = 'heal', -- Holy
+        [258] = 'dps', -- Shadow
     },
     ['Paetun'] = {
-        [262] = 'pvp', -- Elemental
-        [263] = 'pvp', -- Enhancement
-        [264] = 'pve', -- Restoration
+        [262] = 'dps', -- Elemental
+        [263] = 'dps', -- Enhancement
+        [264] = 'heal', -- Restoration
     },
     ['Himitosh'] = {
-        [268] = 'pve', -- Brewmaster
-        [270] = 'pve', -- Mistweaver
-        [269] = 'pvp', -- Windwalker
+        [268] = 'dps', -- Brewmaster
+        [270] = 'heal', -- Mistweaver
+        [269] = 'dps', -- Windwalker
     },
     ['Mirkot'] = {
-        [102] = 'pve', -- Balance
-        [103] = 'pve', -- Feral
-        [104] = 'pve', -- Guardian
-        [105] = 'pve', -- Restoration
+        [102] = 'dps', -- Balance
+        [103] = 'dps', -- Feral
+        [104] = 'dps', -- Guardian
+        [105] = 'heal', -- Restoration
     },
     ['Frutrim'] = {
-        [102] = 'pve', -- Balance
-        [103] = 'pve', -- Feral
-        [104] = 'pve', -- Guardian
-        [105] = 'pve', -- Restoration
+        [102] = 'dps', -- Balance
+        [103] = 'dps', -- Feral
+        [104] = 'dps', -- Guardian
+        [105] = 'heal', -- Restoration
     },
 }
 
